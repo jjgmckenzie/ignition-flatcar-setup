6f4da7eda75bd9289e0f200a31e5f498b55ad08f
James McKenzie <jjgmckenzie@gmail.com>
2024-01-19 14:23:28 -0800

    Update install-flatcar.sh
    Allows key to be defined as env variable beforehand

--- install-flatcar.sh
+++ install-flatcar.sh
@@ -1,9 +1,11 @@
 curl -fsSLO --retry-delay 1 --retry 60 --retry-connrefused --retry-max-time 60 --connect-timeout 20 https://raw.githubusercontent.com/flatcar/init/flatcar-master/bin/flatcar-install
 chmod +x flatcar-install
 
-echo Please enter your SSH Public Key
-read SECRET_SSH_PUBLICKEY
+if [ -z "$SECRET_SSH_PUBLICKEY" ]; then
+  read -p "Please enter your SSH Public Key: " SECRET_SSH_PUBLICKEY
+fi
+
 sed -i "s|SECRET_SSH_PUBLICKEY|$SECRET_SSH_PUBLICKEY|g" config.ignition.json
 
 ./flatcar-install -s -i config.ignition.json
-shutdown -r +1
+echo "Installation completed. Reboot system to finalize."
